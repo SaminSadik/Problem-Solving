5665cd91247da03e35bc00e60fe82cd97934c004
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2023-09-30 14:40:17 +0600

    Add files via upload

---
+++ XPSC/w1_i.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define endl "\n"
+#define SSK ios_base::sync_with_stdio(0); cin.tie(0);
+
+signed main()
+{
+    SSK
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (((a*a)+(b*b) < (c*c)) ? "Yes" : "No");
+
+    return 0;
+}
