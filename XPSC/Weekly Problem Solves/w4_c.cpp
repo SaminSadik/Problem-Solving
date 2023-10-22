7fa44682bc9e0ff93779360119d4c4825ba06479
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2023-10-22 23:48:10 +0600

    Update

--- XPSC/Weekly Problem Solves/w4_c.cpp
+++ XPSC/Weekly Problem Solves/w4_c.cpp
@@ -3,6 +3,51 @@ using namespace std;
 
 //#define int long long
 #define endl "\n"
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int comp(int A, int B)
+{
+    int Count = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        if (((A >> i) & 1) != ((B >> i) & 1))
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+signed main()
+{
+    IOS
+
+    int n, m, k, ans=0;
+    cin >> n >> m >> k;
+    vector<int> x;
+
+    for(int i=0; i<m+1; i++)
+    {
+        int v; cin >> v;
+        x.emplace_back(v);
+    }
+
+    for(int i=0; i<m; i++)
+    {
+        if(comp(x[i], x[m])<=k) ans++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+/* //Another Approach:
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define endl "\n"
 #define SSK ios_base::sync_with_stdio(0); cin.tie(0);
 int n, m, k;
 string Fedor;
@@ -48,3 +93,4 @@ signed main()
 
     return 0;
 }
+*/
