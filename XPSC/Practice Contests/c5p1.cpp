b6ecc78b48b11b6702f7c763a361e98bcb9f9f1d
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2023-11-06 21:43:05 +0600

    Add files via upload

---
+++ XPSC/Practice Contests/c5p1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define endl "\n"
+#define SSK ios_base::sync_with_stdio(0); cin.tie(0);
+
+signed main()
+{
+    SSK
+
+    int x, n, m;
+    cin >> x >> n >> m;
+    cout << ((x+m < n)?"NO":"YES");
+
+    return 0;
+}
---
+++ XPSC/Practice Contests/c5p2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define SSK ios_base::sync_with_stdio(0); cin.tie(0);
+
+signed main()
+{
+    SSK
+
+    int t; cin >> t;
+    while(t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        cout << m/2 + 1 << endl;
+    }
+
+    return 0;
+}
